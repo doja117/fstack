ce85a2edbf90e92d42c1abc45e8a0e1eaac2748b
doja117 <1899doja@gmail.com>
2023-10-13 22:02:20 +0530

    week2.1 done

--- 2.1/index.ts
+++ 2.1/index.ts
@@ -1,4 +1,5 @@
 import ReturnSum from './returnSum';
+import SendTime from './sendTime';
 import express , {Request,Response} from "express";
 
 const app=express();
@@ -9,16 +10,38 @@ function handleRequest(req:Request,resp:Response):Response{
     resp.send("Hello World")
 }
 
+function whenToStart(req:Request,resp:Response){
+    resp.send("week2:1,1:32:32")
+}
+function handleTime(req:Request,resp:Response){
+    var s:string=SendTime();
+    resp.send(`${s}`);
+    (()=>{       
+        setTimeout(handleTime,1000)
+    })()
+    
+}
+
+function handleMathSums(req:Request,reps:Response){
+    console.log(parseInt(req.query.counter))
+    if (!isNaN(parseInt(req.query.counter))){
+    reps.send(`Sum for numbers till ${req.query.counter} is ${ReturnSum(parseInt(req.query.counter))}`)
+    } else{
+    reps.send("Enter Proper Number");
+    }
+}
 function handleSums(req:Request,resp:Response):Response{
     var ans:number=ReturnSum(100);
     resp.send(`Sum for numbers till 100 is ${ans}`);
 }
-
+app.get("/whentostart",whenToStart);
 app.get("/helloWorld",handleRequest);
-app.get("/maths",handleSums);
+app.get("/maths",handleMathSums);
+app.get("/time",handleTime);
 app.get("/",(req:Request,resp:Response)=>{
     resp.send("Welcome to 3000")
 })
+
 
 
 app.listen(port,()=>{
--- 2.1/returnSum.ts
+++ 2.1/returnSum.ts
@@ -1,8 +1,8 @@
 function ReturnSum(n:number):number{
     if (n<0){
-        return -1* (n * (n-1))/2;    
+        return -1* (n * (n+1))/2;    
     }
-    return (n * (n-1))/2;
+    return (n * (n+1))/2;
 }
 
 
